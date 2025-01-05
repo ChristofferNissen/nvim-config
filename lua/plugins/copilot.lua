ab0416b12ab2208fd2ecd01d41c6eb02467ba53d
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-01-05 18:12:02 +0100

    lazyvim config

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        enabled = true,
+        event = "BufEnter",
+        opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+        },
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        enabled = true,
+        opts = {}
+    },
+}/ 
