dd36692403e12ae43f67143ee50764cda54ae98e
Christoffer Nissen <cnis@bankingcircle.com>
2025-01-16 10:16:20 +0100

    markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -45,8 +45,10 @@ return {
 	-- 	end,
 	-- },
 	{
-		"MeanderingProgrammer/markdown.nvim",
+		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
 		opts = {},
 		ft = { "markdown" },
 		keys = {
