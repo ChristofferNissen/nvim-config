ddb9b563f934a155c2b754d5ce3aecd73c0c1675
Christoffer Nissen <cnis@bankingcircle.com>
2025-01-19 19:23:56 +0100

    rust refactor

--- shell.nix
+++ shell.nix
@@ -3,7 +3,14 @@
 pkgs.mkShell {
   shellHook = ''
     mkdir -p ~/.config/local/lua/ChristofferNissen
+
+    rm -f ~/.config/local/lua/ChristofferNissen/init.lua
     touch ~/.config/local/lua/ChristofferNissen/init.lua 
+    echo "require('ChristofferNissen.set')" >> ~/.config/local/lua/ChristofferNissen/init.lua
+    echo "require('ChristofferNissen.remap')" >> ~/.config/local/lua/ChristofferNissen/init.lua
+    echo "require('ChristofferNissen.options')" >> ~/.config/local/lua/ChristofferNissen/init.lua 
+    echo "" >> ~/.config/local/lua/ChristofferNissen/init.lua
+
     ln -sf ~/configs/lazyvim-config/lua/ChristofferNissen/* ~/.config/local/lua/ChristofferNissen/
     ln -sf ~/configs/lazyvim-config/init.lua ~/.config/local/init.lua
     ln -sf ~/configs/lazyvim-config/lua/plugins/* ~/.config/local/lua/plugins/
