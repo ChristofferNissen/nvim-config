9cd5f22c6465e6f28045001fbb53c9d4ca438991
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-03-06 23:16:31 +0100

    add keybinds for telescope terraform extensions

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-# lazyvim-config
+# lazynvim-config
 
-My LazyVim configuration.
+My Neovim configuration is built with the [lazy.nvim](https://lazy.folke.io/) package manager.
+## Adding a plugin
+
+Plugins are normally defined in `lua/plugins.lua`. In order to make the config
+more maintainable, I've added a `lua/plugins/*.lua` file for each plugin.
+
+Any lua file in `lua/plugins/*.lua` will be automatically merged in the main
+plugin spec.
+
+## Testing
+
+You can test config changes by running `task test` which will setup a test config folder under `~/.config`.
+
+You can check the health in the new config with:
+
+```vim
+:checkhealth lazy
+```
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,40 +1,28 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        lazy = false,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "fredrikaverpil/godoc.nvim",
-            {
-                "nvim-telescope/telescope-file-browser.nvim",
-                event = "VeryLazy",
-                dependencies = {
-                    "nvim-telescope/telescope.nvim",
-                    "nvim-lua/plenary.nvim",
-                    "nvim-tree/nvim-web-devicons",
-                },
-            },
-        },
-        specs = {
-            {
-                "ANGkeith/telescope-terraform-doc.nvim",
-                ft = { "terraform", "hcl" },
-                config = function()
-                    require("lazyvim").on_load("telescope.nvim", function()
-                        require("telescope").load_extension("terraform_doc")
-                    end)
-                end,
-            },
-            {
-                "cappyzawa/telescope-terraform.nvim",
-                ft = { "terraform", "hcl" },
-                config = function()
-                    require("lazyvim").on_load("telescope.nvim", function()
-                        require("telescope").load_extension("terraform")
-                    end)
-                end,
-            },
-        },
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"fredrikaverpil/godoc.nvim",
+			{
+				"nvim-telescope/telescope-file-browser.nvim",
+				event = "VeryLazy",
+				dependencies = {
+					"nvim-telescope/telescope.nvim",
+					"nvim-lua/plenary.nvim",
+					"nvim-tree/nvim-web-devicons",
+				},
+			},
+			{
+				"ANGkeith/telescope-terraform-doc.nvim",
+				dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+			},
+			{
+				"cappyzawa/telescope-terraform.nvim",
+				dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+			},
+		},
         -- stylua: ignore
         keys = {
             {
@@ -88,20 +76,34 @@ return {
                 mode = "n",
                 function() require("godoc").show_telescope_picker() end,
                 desc = "telescope godoc",
+            },
+            {
+                "<leader>ptt",
+                mode = "n",
+                function() require("telescope").extensions.terraform.state_list() end,
+                desc = "telescope terraform",
+            },
+            {
+                "<leader>ptd",
+                mode = "n",
+                function() require("telescope").extensions.terraform_doc.terraform_doc() end,
+                desc = "telescope terraform docs",
             }
         },
-        config = function()
-            local opts = {
-                extensions = {
-                    file_browser = {
-                        respect_gitignore = false,
-                        hijack_netrw = true,
-                        hidden = true,
-                    },
-                },
-            }
-            require("telescope").setup(opts)
-            require("telescope").load_extension("file_browser")
-        end,
-    },
+		config = function()
+			local opts = {
+				extensions = {
+					file_browser = {
+						respect_gitignore = false,
+						hijack_netrw = true,
+						hidden = true,
+					},
+				},
+			}
+			require("telescope").setup(opts)
+			require("telescope").load_extension("file_browser")
+			require("telescope").load_extension("terraform_doc")
+			require("telescope").load_extension("terraform")
+		end,
+	},
 }
