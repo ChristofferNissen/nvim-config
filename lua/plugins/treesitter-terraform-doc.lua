7678b8e99a5f110adcc7804fc30d2921ecd8366e
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-03-07 13:33:25 +0100

    change browser to w3m for treesitter tf doc extension

--- lua/plugins/treesitter-terraform-doc.lua
+++ lua/plugins/treesitter-terraform-doc.lua
@@ -1,10 +1,10 @@
 return {
-	"Afourcat/treesitter-terraform-doc.nvim",
-	config = function()
-		require("treesitter-terraform-doc").setup({
-			command_name = "OpenDoc",
-			url_opener_command = "!ffirefox", -- or your preferred browser command
-			jump_argument = true,
-		})
-	end,
+    "Afourcat/treesitter-terraform-doc.nvim",
+    config = function()
+        require("treesitter-terraform-doc").setup({
+            command_name = "OpenDoc",
+            url_opener_command = "!w3m", -- or your preferred browser command
+            jump_argument = true,
+        })
+    end,
 }
