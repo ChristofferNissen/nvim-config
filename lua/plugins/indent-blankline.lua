e0403f51688995434b0e46002dfbc3ad68df0a4c
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-03-11 15:29:57 +0100

    indent-blankline restore old config

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,9 +1,12 @@
 return {
     {
         "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {},
+        tag = "v2.20.8",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            space_char_blankline = " ",
+            show_current_context = true,
+            show_current_context_start = true,
+        },
     },
 }
