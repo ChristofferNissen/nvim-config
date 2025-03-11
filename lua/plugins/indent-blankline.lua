ab2a7584b46f2f50aef60601e3241e07fe44f694
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-03-11 15:19:18 +0100

    indent-blankline update

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,11 +1,9 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		---@module "ibl"
-		---@type ibl.config
-		opts = function()
-			require("ibl").setup()
-		end,
-	},
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+    },
 }
