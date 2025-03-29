bbca21cd82b5c5c5869186cd814c26fc06438128
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-03-29 17:14:01 +0100

    add neotree and make oil non-default

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+        -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+        -- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+        default_file_explorer = true,
+    },
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
