360f8771cc2a9c529c890c0d7f921144a1190cfd
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-04-08 18:41:23 +0200

    update tf ls filetypes

--- lsp/terraform-ls.lua
+++ lsp/terraform-ls.lua
@@ -1,7 +1,7 @@
 ---@type vim.lsp.Config
 return {
     cmd = { "terraform-ls", "serve" },
-    filetypes = { "hcl" },
+    filetypes = { "hcl", "terraform", "tf" },
     on_attach = function()
         require("treesitter-terraform-doc").setup({})
         -- Other on_attach configurations
