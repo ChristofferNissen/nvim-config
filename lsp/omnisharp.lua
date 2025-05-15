b79bd3b681814dfd5c85d46fbfe5a21b1ed83da9
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-05-15 20:44:17 +0200

    remove roslyn plugin

--- lsp/omnisharp.lua
+++ lsp/omnisharp.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { 'OmniSharp' },
+    cmd = { 'OmniSharp', '-lsp' },
     filetypes = { 'sln', "cs" },
     -- root_markers = { 'azure-pipelines.yml' },
     settings = {},
