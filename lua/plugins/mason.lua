889a125653965c196ddf0ed77e12b74e1421f048
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-05-17 18:54:24 +0200

    move programs from nix to mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,32 @@
 return {
     "mason-org/mason.nvim",
-    opts = { ensure_installed = { "csharpier", "netcoredbg" } },
+    opts = {
+        ensure_installed = {
+            "yaml-language-server",
+            "dockerfile-language-server",
+            "docker-compose-language-service",
+            "lua-language-server",
+            -- c#
+            "omnisharp",
+            "csharp-language-server",
+            "csharpier",
+            "netcoredbg",
+            "azure-pipelines-language-server",
+            -- terraform
+            "terraform-ls",
+            "tflint",
+            -- zig
+            "zls",
+            -- markdown
+            "marksman",
+            "markdownlint-cli2",
+            "mermaid-cli",
+            -- go
+            "gopls",
+            "gosimports",
+            "gofumpt",
+            "gomodifytags",
+            "impl",
+        }
+    },
 }
