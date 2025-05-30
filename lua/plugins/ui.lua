afd4c0b78c994a5362c6261ebeaeeb148118bdfe
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-05-30 19:54:37 +0200

    fix noice warnings

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -118,6 +118,12 @@ return {
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+				},
+			},
 			-- add any options here
 		},
 		dependencies = {
