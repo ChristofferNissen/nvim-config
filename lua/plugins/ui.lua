eae698fdca6a1702ff1ad2a308d5b5ad46b38f30
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-05-30 19:37:43 +0200

    add snacks picker

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,14 @@
 return {
 	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			picker = { enabled = true },
+		},
+	},
+	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		opts = {},
