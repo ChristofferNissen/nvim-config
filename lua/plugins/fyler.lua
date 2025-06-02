9d3a1a341762e4e12af92113ed207add3ffd0ea0
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-06-02 23:07:15 +0200

    change fyler to follow development

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -1,6 +1,7 @@
 return {
     {
         "ChristofferNissen/fyler.nvim",
+        branch = "development",
         dependencies = { "echasnovski/mini.icons" },
         opts = {
             hijack_netrw = true,
