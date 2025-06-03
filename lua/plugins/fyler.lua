5cb965cc9c046d3754ea314c6eee59b021d117c6
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-06-04 00:10:31 +0200

    update fyler to new beta

--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -1,10 +1,13 @@
 return {
     {
         "ChristofferNissen/fyler.nvim",
-        branch = "development",
+        branch = "feat/reveal-at-file-path",
+        -- dev = true,
+        -- dir = "~/code/fyler.nvim/",
         dependencies = { "echasnovski/mini.icons" },
         opts = {
             hijack_netrw = true,
+            close_on_file_open = true,
         },
     },
 }
