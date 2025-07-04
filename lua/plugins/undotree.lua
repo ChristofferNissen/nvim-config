052f1e04aa4f820fd32694fb7bdd359f10c81617
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-07-04 17:39:18 +0200

    add undotree

---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "mbbill/undotree",
+        cmd = { "UndotreeToggle", "UndotreeShow" },
+        config = function()
+            -- Optional: your custom Undotree settings here
+            -- Example:
+            -- vim.g.undotree_WindowLayout = 2
+        end,
+    },
+}
