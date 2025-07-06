6086edc9a30ff0f9d9f14730750bc08f66d7ea41
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-07-06 21:51:34 +0200

    add plugin for java lsp

---
+++ lua/plugins/nvim-jdtls.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "mfussenegger/nvim-jdtls",
+        ft = { "java" },
+        config = function()
+            -- You can require and configure nvim-jdtls here, e.g.:
+            -- require('jdtls').start_or_attach({ ... })
+        end,
+    },
+}
