614fbe52399deaee6dacccecb580cfc0886d87c7
Christoffer Nissen <christoffer.nissen@gmail.com>
2025-07-21 21:12:02 +0200

    fix bashls root dir error

--- lsp/bashls.lua
+++ lsp/bashls.lua
@@ -14,8 +14,5 @@ return {
         },
     },
     filetypes = { "bash", "sh" },
-    root_dir = function(fname)
-        return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
-    end,
     single_file_support = true,
 }
