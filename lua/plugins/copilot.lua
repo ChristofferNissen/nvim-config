a9c76a3c84e4ff586d878ab47b687b64a3977ab9
Christoffer Nissen <christoffer.nissen@gmail.com>
2026-02-01 17:10:25 +0100

    remove copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -12,18 +12,5 @@ return {
                 help = true,
             },
         },
-    },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        dependencies = {
-            { "zbirenbaum/copilot.lua" },
-            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-        },
-        build = "make tiktoken", -- Only on MacOS or Linux
-        opts = {
-            -- See Configuration section for options
-            -- system_prompt = vim.fn.fnamemodify(vim.env.MYVIMRC, ":h") .. "/lua/plugins/prompts/4.1.chatmode.md",
-        },
-        -- See Commands section for default commands if you want to lazy load on them
-    },
+    }
 }
