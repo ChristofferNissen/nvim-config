dd78f7f77c05225b8349278d37ecc3bed14e7eca
Christoffer Nissen <christoffer.nissen@gmail.com>
2026-05-07 10:06:55 +0200

    disable textobjects treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,15 +5,15 @@ return {
         main = "nvim-treesitter.configs",
         branch = "main",
         dependencies = {
-            {
-                "nvim-treesitter/nvim-treesitter-textobjects",
-                dependencies = { "nvim-treesitter/nvim-treesitter" },
-                config = function()
-                    -- You can add your Treesitter textobjects config here if desired
-                    -- For example, enable specific modules in require("nvim-treesitter.configs").setup
-                end,
-                event = "VeryLazy", -- or "BufReadPre" or your preferred lazy-load event
-            },
+            -- {
+            --     "nvim-treesitter/nvim-treesitter-textobjects",
+            --     dependencies = { "nvim-treesitter/nvim-treesitter" },
+            --     config = function()
+            --         -- You can add your Treesitter textobjects config here if desired
+            --         -- For example, enable specific modules in require("nvim-treesitter.configs").setup
+            --     end,
+            --     event = "VeryLazy", -- or "BufReadPre" or your preferred lazy-load event
+            -- },
             {
                 "Afourcat/treesitter-terraform-doc.nvim",
                 ft = { "terraform", "hcl"},
